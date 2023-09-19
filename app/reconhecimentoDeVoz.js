b40b6aa00dc63e0bdceb51808cfa968513950e72
Tiago Jerônimo <tiago.batmansz@gmail.com>
2023-09-19 15:05:45 -0300

    Implementando sorteador e api de reconhecimento de voz

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,4 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br';
+recognition.start();
--- app/sorterNumero.js
+++ app/sorterNumero.js
@@ -1,7 +1,18 @@
+const menorValor = 1;
+const maiorValor = 100;
 const numeroSecreto = gerarNumeroAleatorio();
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 100)
+    return parseInt(Math.random() * maiorValor + 1)
 }
+
+
+
+const elementoMenorValor = document.getElementById('menor-valor');  
+elementoMenorValor.innerHTML = menorValor;
+
+const elementoMaiorValor = document.getElementById('maior-valor');  
+elementoMaiorValor.innerHTML = maiorValor;
+
 
 console.log(`Número secreto: ${numeroSecreto}`); 
