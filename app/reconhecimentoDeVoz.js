80ea1e2f66021d4b06daf56de39de004abae08d8
Tiago Jerônimo <tiago.batmansz@gmail.com>
2023-09-19 16:38:58 -0300

    Mostrando a mensagem na tela

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,4 +1,19 @@
+const elementoChute = document.getElementById('chute');
+
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
+
 recognition.lang = 'pt-Br';
 recognition.start();
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(event) {
+    chute = event.results[0][0].transcript
+    exibeChuteNaTela(chute)
+}
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML =`
+    <div> Você disse</div>
+    <span class="box">${chute}</`
+}
